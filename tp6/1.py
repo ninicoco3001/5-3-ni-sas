2d5e3bebd677918a9e5fd3ca4ef1654bf4813d3d
ninicoco3001 <gooler3001@gmail.com>
2025-06-13 09:24:03 -0300

    l

---
+++ tp6/1.py
@@ -0,0 +1,37 @@
+lista = ["victoria mayer",
+          "emilia balague", 
+          "azul benitez", 
+          "martina bednarek", 
+          "bianca cugno", 
+          "valentina diez",
+            "bianca farriol",
+         "brenda graff",
+          "avril garcia",
+            "candelaria herrera",
+           "candela salinas",
+         "daniela bulaich", 
+         "bianca bertolino", 
+         "nicole perez",
+           "elina rodriguez", 
+           "antonela fortuna", 
+           "agostina pelozo", 
+           "sol onofri"]
+print (lista)
+print("b")
+longitud = len(lista)
+print(longitud)
+print("c")
+print(lista[0])
+print(lista[4])
+print(lista[3])
+print(lista[11])
+print(lista[10])
+print(lista[9])
+print("d")
+print (lista[9])
+#e
+lista.pop(3)
+#f
+lista.remove("bianca cugno")
+print("g")
+print(longitud)
