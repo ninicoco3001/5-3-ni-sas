76a15d134fbf7334dadc54663ce56806e40df903
ninicoco3001 <gooler3001@gmail.com>
2025-08-22 09:23:54 -0300

    k

--- tp6.2/1.py
+++ tp6.2/1.py
@@ -1,4 +1,27 @@
 secciones = []
 votos = []
 
-print("queres agregar una nueva seccion, ¿y o n?")
+opcion = input("queres agregar una nueva seccion, ¿y o n?")
+
+while (opcion == "y"):
+    nuevas = input("cual es la nueva seccion que vas a meter???????????????")
+    secciones.append(nuevas)
+    nuevav = input("cual es la cantidad de votos")
+    votos.append(nuevav)
+    opcion = input("queres agregar una nueva seccion, ¿y o n?")
+
+#A
+longitud = len(secciones)
+print("a continuacion, la cantidad de secciones")
+print(longitud)
+
+#B
+for p in range(0, len(votos)):
+    total = total + votos[p]
+
+#c
+valor = 0
+for i in range(0, len(votos)):
+    if (valor < votos[i]):
+        m = listas[i]
+        posicion = i
